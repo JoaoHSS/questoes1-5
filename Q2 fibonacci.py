7507a6f8f870c36ff1cd4dacdef9ecd32cc457ae
João Henrique dos Santos Silva <88688141+JoaoHSS@users.noreply.github.com>
2023-03-23 11:12:40 -0300

    Update Q2 fibonacci.py

--- Q2 fibonacci.py
+++ Q2 fibonacci.py
@@ -1,4 +1,4 @@
-
+#   solução usando loop
 def fibonacci(alvo):
     a, b = 1, 0
     while b < alvo:
@@ -8,16 +8,26 @@ def fibonacci(alvo):
         if b == alvo: return True
     return False
 
+#   solução usando recursividade
+def firstCheck(number):
+    if number < 4: return True
+    return isFib(number)
+
+def isFib(number, a=1, b=1):
+    next = a + b
+    if next > number:   return False
+    elif number == next: return True
+    return isFib(number, b, next)
 
 while True:
-    a = input(" Digite um númeoro para eu conferir se está no fibonacci:")
+    a = input(" Digite um número para conferir se está na sequência fibonacci:\n")
     try: 
         a = int(a)
         break
     except: print(" Digite um número válido")
 
-
 resultado = fibonacci(a)
+#resultado = firstCheck(a)
 
 if resultado:
     print(f"o número {a} pertence a sequência fibonacci")
